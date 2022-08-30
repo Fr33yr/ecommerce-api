8fa853dfaa37e52e420f3a7077f61686d6e03178
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-08-30 17:01:06 -0300

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,3 +1,3 @@
-const MONGODB_URI = //mongodb url
+const MONGODB_URI = "mongodb+srv://fr33yr:PkRRCUcnEbYZvMJe@cluster0.hxfdpbp.mongodb.net/e-commerce?retryWrites=true&w=majority" 
 
 module.exports = MONGODB_URI
